13f4e74874a8c62b603f26639fe51378f91329a6
Roger Howell <howelrtc@aston.ac.uk>
2020-02-15 15:01:54 +0000

    added basic panel contents with content / panel details - closes #2

--- src/main/java/com/github/rogerhowell/JavaCodeBrowser/tool_window/AstBrowserToolWindowFactory.java
+++ src/main/java/com/github/rogerhowell/JavaCodeBrowser/tool_window/AstBrowserToolWindowFactory.java
@@ -2,13 +2,97 @@ package com.github.rogerhowell.JavaCodeBrowser.tool_window;
 
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.ToolWindowFactory;
+import com.intellij.ui.content.Content;
+import com.intellij.ui.content.ContentManager;
 import org.jetbrains.annotations.NotNull;
 
-public class AstBrowserToolWindowFactory implements com.intellij.openapi.wm.ToolWindowFactory {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+
+public class AstBrowserToolWindowFactory implements ToolWindowFactory {
+
+    /**
+     * https://www.jetbrains.org/intellij/sdk/docs/user_interface_components/tool_windows.html
+     * In addition to that, you specify the factory class - the name of a class implementing the ToolWindowFactory interface.
+     *
+     * When the user clicks on the tool window button, the createToolWindowContent() method of the factory class is called,
+     * and initializes the UI of the tool window.
+     *
+     * This procedure ensures that unused tool windows don’t cause any overhead in startup time or memory usage: if a user
+     * does not interact with the tool window of your plugin, no plugin code will be loaded or executed.
+     *
+     * @param project
+     * @param toolWindow
+     */
     @Override
     public void createToolWindowContent(@NotNull final Project project, @NotNull final ToolWindow toolWindow) {
 
         CodeGraphBrowserToolWindow codeGraphBrowserToolWindow = new CodeGraphBrowserToolWindow(toolWindow);
 
+        JTabbedPane tabbedPane = new JTabbedPane();
+        ImageIcon icon = createImageIcon("/JavaCodeBrowser/graph icon -- 13x13.png");
+
+        JComponent panel1 = makeTextPanel("Panel #1");
+        tabbedPane.addTab("Tab 1", icon, panel1,
+                          "Does nothing");
+        tabbedPane.setMnemonicAt(0, KeyEvent.VK_1);
+
+        JComponent panel2 = makeTextPanel("Panel #2");
+        tabbedPane.addTab("Tab 2", icon, panel2,
+                          "Does twice as much nothing");
+        tabbedPane.setMnemonicAt(1, KeyEvent.VK_2);
+
+        JComponent panel3 = makeTextPanel("Panel #3");
+        tabbedPane.addTab("Tab 3", icon, panel3,
+                          "Still does nothing");
+        tabbedPane.setMnemonicAt(2, KeyEvent.VK_3);
+
+        JComponent panel4 = makeTextPanel(
+                "Panel #4 (has a preferred size of 410 x 50).");
+        panel4.setPreferredSize(new Dimension(410, 50));
+        tabbedPane.addTab("Tab 4", icon, panel4,
+                          "Does nothing at all");
+        tabbedPane.setMnemonicAt(3, KeyEvent.VK_4);
+
+        /*
+         * https://www.jetbrains.org/intellij/sdk/docs/user_interface_components/tool_windows.html
+         * As mentioned previously, tool windows can contain multiple tabs, or contents.
+         * To manage the contents of a tool window, you can call ToolWindow.getContentManager().
+         * To add a tab (content), you first need to create it by calling ContentManager.getFactory().createContent(),
+         * and then to add it to the tool window using ContentManager.addContent().
+         */
+//        JComponent jComponent = null;
+        String displayName = "DisplayName213";
+        boolean isLockable = false;
+
+        final ContentManager contentManager = toolWindow.getContentManager();
+        final Content newTab1 = contentManager.getFactory().createContent(tabbedPane, "Display Name 123", false);
+        final Content newTab2 = contentManager.getFactory().createContent(tabbedPane, "Display Name 321", false);
+        contentManager.addContent(newTab1);
+        contentManager.addContent(newTab2);
     }
+
+
+    protected JComponent makeTextPanel(String text) {
+        JPanel panel = new JPanel(false);
+        JLabel filler = new JLabel(text);
+        filler.setHorizontalAlignment(JLabel.CENTER);
+        panel.setLayout(new GridLayout(1, 1));
+        panel.add(filler);
+        return panel;
+    }
+
+    /** Returns an ImageIcon, or null if the path was invalid. */
+    protected static ImageIcon createImageIcon(String path) {
+        java.net.URL imgURL = AstBrowserToolWindowFactory.class.getResource(path);
+        if (imgURL != null) {
+            return new ImageIcon(imgURL);
+        } else {
+            System.err.println("Couldn't find file: " + path);
+            return null;
+        }
+    }
+
 }
