2a9208bea81f11cb5d4740bb89ef5ee4f057015d
Roger Howell <howelrtc@aston.ac.uk>
2020-02-15 13:35:30 +0000

    basic use of javaparser to parse the "current" project based on the source roots given by intellij - closes #7

--- src/main/java/com/github/rogerhowell/JavaCodeBrowser/BasicAction.java
+++ src/main/java/com/github/rogerhowell/JavaCodeBrowser/BasicAction.java
@@ -1,5 +1,8 @@
 package com.github.rogerhowell.JavaCodeBrowser;
 
+import com.github.javaparser.ParseResult;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.utils.SourceRoot;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.project.Project;
@@ -8,7 +11,10 @@ import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.vfs.VirtualFile;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class BasicAction extends AnAction {
@@ -24,9 +30,47 @@ public class BasicAction extends AnAction {
         VirtualFile[] vFiles          = ProjectRootManager.getInstance(project).getContentSourceRoots();
         String        sourceRootsList = Arrays.stream(vFiles).map(VirtualFile::getUrl).collect(Collectors.joining("\n"));
 
-        Messages.showInfoMessage(
-                "Source roots for the " + projectName + " plugin:\n" + sourceRootsList,
-                "Project Properties"
-        );
+//        Messages.showInfoMessage(
+//                "Source roots for the " + projectName + " plugin:\n" + sourceRootsList,
+//                "Project Properties"
+//        );
+
+
+        final List<SourceRoot> roots = Arrays.stream(vFiles)
+                                             .map(VirtualFile::getPath)
+                                             .map(Paths::get)
+                                             .map(SourceRoot::new)
+                                             .collect(Collectors.toList());
+
+        StringBuilder sb = new StringBuilder(1000);
+//        List<ParseResult<CompilationUnit>> allParseResults = new ArrayList<>();
+
+        for (int i = 0; i < roots.size(); i++) {
+            SourceRoot sourceRoot = roots.get(i);
+            try {
+                List<ParseResult<CompilationUnit>> parseResults = sourceRoot.tryToParse();
+//                allParseResults.addAll(parseResults);
+
+                sb.append("\n");
+                sb.append("\n");
+                sb.append(" === source root #" + i + ": " + sourceRoot.getRoot().toString() + " === ");
+                sb.append("\n");
+                for (int j = 0; j < parseResults.size(); j++) {
+                    ParseResult<CompilationUnit> parseResult = parseResults.get(j);
+                    sb.append("    ");
+                    sb.append(i + "." + j + " - " + parseResult.isSuccessful());
+                    sb.append(": ");
+                    sb.append(sourceRoot.getRoot().toString());
+                    sb.append("\n");
+                }
+
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+
+        }
+
+        Messages.showMessageDialog(e.getProject(), sb.toString(), "Test Title", Messages.getInformationIcon());
+
     }
 }
