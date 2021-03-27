7eae853b24da596eb09a8288948f2be5d1ae7059
Roger Howell <MysterAitch@users.noreply.github.com>
2021-03-27 21:39:41 +0000

    update, based on latest intellij plugin template (including removal of bintray)
    https://github.com/JetBrains/intellij-platform-plugin-template

--- README.md
+++ README.md
@@ -1,18 +1,35 @@
-[ ![GitHub release (latest by date including pre-releases)](https://img.shields.io/github/v/release/MysterAitch/JavaParser-AST-Inspector?include_prereleases) ](https://github.com/mysteraitch/javaparser-ast-inspector/releases)
-[ ![Download from Bintray](https://api.bintray.com/packages/mysteraitch/JavaParser-AST-Inspector/JavaParser-AST-Inspector/images/download.svg?&label=Bintray) ](https://bintray.com/mysteraitch/JavaParser-AST-Inspector/JavaParser-AST-Inspector/_latestVersion)
+
+# JavaParser AST Inspector
+
+<!-- Note: PLUGIN_ID=14245 -->
+![Build](https://github.com/MysterAitch/JavaParser-AST-Inspector/workflows/Build/badge.svg)
+[![Version](https://img.shields.io/jetbrains/plugin/v/14245.svg)](https://plugins.jetbrains.com/plugin/14245)
+[![Downloads](https://img.shields.io/jetbrains/plugin/d/14245.svg)](https://plugins.jetbrains.com/plugin/14245)
 [![GitHub license](https://img.shields.io/github/license/MysterAitch/JavaParser-AST-Inspector)](https://github.com/MysterAitch/JavaParser-AST-Inspector/blob/master/LICENSE)
 [![GitHub forks](https://img.shields.io/github/forks/MysterAitch/JavaParser-AST-Inspector)](https://github.com/MysterAitch/JavaParser-AST-Inspector/network)
 [![GitHub stars](https://img.shields.io/github/stars/MysterAitch/JavaParser-AST-Inspector)](https://github.com/MysterAitch/JavaParser-AST-Inspector/stargazers)
 
 
 
-# JavaParser AST Inspector
-
 <!-- Plugin description -->
-Browse and navigate the Abstract Syntax Tree (AST) based on the Java code in your editor. 
+Browse and navigate the Abstract Syntax Tree (AST) based on the Java code in your editor.
 
-Uses JavaParser v3.15.21 which currently handles Java 1-14, with Java 15 coming soon!
+Uses JavaParser v3.17.0 which currently handles Java 1-15, with Java 16 coming soon!
 <!-- Plugin description end -->
+
+
+## Installation
+
+- Using IDE built-in plugin system:
+  
+  <kbd>Settings/Preferences</kbd> > <kbd>Plugins</kbd> > <kbd>Marketplace</kbd> > <kbd>Search for "JavaParser-AST-Inspector"</kbd> >
+  <kbd>Install Plugin</kbd>
+  
+- Manually:
+
+  Download the [latest release](https://github.com/MysterAitch/JavaParser-AST-Inspector/releases/latest) and install it manually using
+  <kbd>Settings/Preferences</kbd> > <kbd>Plugins</kbd> > <kbd>⚙️</kbd> > <kbd>Install plugin from disk...</kbd>
+
 
 
 ## Hackathon Notes
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,58 +1,36 @@
-import com.jfrog.bintray.gradle.BintrayExtension
 import io.gitlab.arturbosch.detekt.Detekt
 import org.jetbrains.changelog.closure
 import org.jetbrains.changelog.markdownToHTML
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+fun properties(key: String) = project.findProperty(key).toString()
+
 plugins {
     // Java support
     id("java")
     // Kotlin support
-    id("org.jetbrains.kotlin.jvm") version "1.4.20"
+    id("org.jetbrains.kotlin.jvm") version "1.4.31"
     // gradle-intellij-plugin - read more: https://github.com/JetBrains/gradle-intellij-plugin
-    id("org.jetbrains.intellij") version "0.6.4"
+    id("org.jetbrains.intellij") version "0.7.2"
     // gradle-changelog-plugin - read more: https://github.com/JetBrains/gradle-changelog-plugin
-    id("org.jetbrains.changelog") version "0.6.2"
+    id("org.jetbrains.changelog") version "1.1.2"
     // detekt linter - read more: https://detekt.github.io/detekt/gradle.html
-    id("io.gitlab.arturbosch.detekt") version "1.14.2"
-
-    id("com.jfrog.bintray") version "1.8.5"
+    id("io.gitlab.arturbosch.detekt") version "1.15.0"
+    // ktlint linter - read more: https://github.com/JLLeitschuh/ktlint-gradle
+    id("org.jlleitschuh.gradle.ktlint") version "10.0.0"
 }
 
-// Import variables from gradle.properties file
-val pluginGroup: String by project
-// `pluginName_` variable ends with `_` because of the collision with Kotlin magic getter in the `intellij` closure.
-// Read more about the issue: https://github.com/JetBrains/intellij-platform-plugin-template/issues/29
-val pluginName_: String by project
-val pluginVersion: String by project
-val pluginSinceBuild: String by project
-val pluginUntilBuild: String by project
-val pluginVerifierIdeVersions: String by project
-
-val platformType: String by project
-val platformVersion: String by project
-val platformPlugins: String by project
-val platformDownloadSources: String by project
-
-group = pluginGroup
-version = pluginVersion
+group = properties("pluginGroup")
+version = properties("pluginVersion")
 
 val javaVersion = "1.8"
-//val javaVersion = JavaVersion.VERSION_1_8
-//val sourceCompatibilityVersion = javaVersion
-//val targetCompatibilityVersion = javaVersion
-
-val artifactName = project.name
-val artifactGroup = project.group.toString()
-val artifactVersion = project.version.toString()
 
 // Configure project's dependencies
 repositories {
     mavenCentral()
 }
-
 dependencies {
-    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.14.2")
+    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.15.0")
     implementation("com.github.javaparser:javaparser-symbol-solver-core:3.17.0")
     implementation("org.apache.commons:commons-text:1.9")
     implementation("guru.nidi:graphviz-java-all-j2v8:0.18.0")
@@ -64,14 +42,21 @@ dependencies {
 // Configure gradle-intellij-plugin plugin.
 // Read more: https://github.com/JetBrains/gradle-intellij-plugin
 intellij {
-    pluginName = pluginName_
-    version = platformVersion
-    type = platformType
-    downloadSources = platformDownloadSources.toBoolean()
+    pluginName = properties("pluginName")
+    version = properties("platformVersion")
+    type = properties("platformType")
+    downloadSources = properties("platformDownloadSources").toBoolean()
     updateSinceUntilBuild = true
 
     // Plugin Dependencies. Uses `platformPlugins` property from the gradle.properties file.
-    setPlugins(*platformPlugins.split(',').map(String::trim).filter(String::isNotEmpty).toTypedArray())
+    setPlugins(*properties("platformPlugins").split(',').map(String::trim).filter(String::isNotEmpty).toTypedArray())
+}
+
+// Configure gradle-changelog-plugin plugin.
+// Read more: https://github.com/JetBrains/gradle-changelog-plugin
+changelog {
+    version = properties("pluginVersion")
+    groups = emptyList()
 }
 
 // Configure detekt plugin.
@@ -88,15 +73,13 @@ detekt {
 }
 
 tasks {
-    // Set the compatibility versions
+    // Set the compatibility versions to 1.8
     withType<JavaCompile> {
         sourceCompatibility = javaVersion
         targetCompatibility = javaVersion
     }
-    listOf("compileKotlin", "compileTestKotlin").forEach {
-        getByName<KotlinCompile>(it) {
-            kotlinOptions.jvmTarget = javaVersion
-        }
+    withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = javaVersion
     }
 
     withType<Detekt> {
@@ -104,35 +87,35 @@ tasks {
     }
 
     patchPluginXml {
-        version(pluginVersion)
-        sinceBuild(pluginSinceBuild)
-        untilBuild(pluginUntilBuild)
+        version(properties("pluginVersion"))
+        sinceBuild(properties("pluginSinceBuild"))
+        untilBuild(properties("pluginUntilBuild"))
 
         // Extract the <!-- Plugin description --> section from README.md and provide for the plugin's manifest
         pluginDescription(
-                closure {
-                    File("./README.md").readText().lines().run {
-                        val start = "<!-- Plugin description -->"
-                        val end = "<!-- Plugin description end -->"
+            closure {
+                File("./README.md").readText().lines().run {
+                    val start = "<!-- Plugin description -->"
+                    val end = "<!-- Plugin description end -->"
 
-                        if (!containsAll(listOf(start, end))) {
-                            throw GradleException("Plugin description section not found in README.md:\n$start ... $end")
-                        }
-                        subList(indexOf(start) + 1, indexOf(end))
-                    }.joinToString("\n").run { markdownToHTML(this) }
-                }
+                    if (!containsAll(listOf(start, end))) {
+                        throw GradleException("Plugin description section not found in README.md:\n$start ... $end")
+                    }
+                    subList(indexOf(start) + 1, indexOf(end))
+                }.joinToString("\n").run { markdownToHTML(this) }
+            }
         )
 
         // Get the latest available change notes from the changelog file
         changeNotes(
-                closure {
-                    changelog.getLatest().toHTML()
-                }
+            closure {
+                changelog.getLatest().toHTML()
+            }
         )
     }
 
     runPluginVerifier {
-        ideVersions(pluginVerifierIdeVersions)
+        ideVersions(properties("pluginVerifierIdeVersions"))
     }
 
     publishPlugin {
@@ -140,65 +123,7 @@ tasks {
         token(System.getenv("INTELLIJ_PLUGIN_PUBLISH_TOKEN"))
         // pluginVersion is based on the SemVer (https://semver.org) and supports pre-release labels, like 2.1.7-alpha.3
         // Specify pre-release label to publish the plugin in a custom Release Channel automatically. Read more:
-        // https://jetbrains.org/intellij/sdk/docs/tutorials/build_system/deployment.html#specifying-a-release-channel
-        channels(pluginVersion.split('-').getOrElse(1) { "default" }.split('.').first())
+        // https://plugins.jetbrains.com/docs/intellij/deployment.html#specifying-a-release-channel
+        channels(properties("pluginVersion").split('-').getOrElse(1) { "default" }.split('.').first())
     }
-}
-
-
-// Configure detekt plugin.
-// Read more: https://detekt.github.io/detekt/kotlindsl.html
-detekt {
-    config = files("./detekt-config.yml")
-    buildUponDefaultConfig = true
-
-    reports {
-        html.enabled = false
-        xml.enabled = false
-        txt.enabled = false
-    }
-}
-
-
-//changelog {
-//    version = "${project.version}"
-//    path = "${project.projectDir}/CHANGELOG.md"
-//    header = closure { "[${project.version}] - ${date()}" }
-//    headerParserRegex = """\d+\.\d+\.\d+""".toRegex()
-//    itemPrefix = "-"
-//    keepUnreleasedSection = true
-//    unreleasedTerm = "[Unreleased]"
-//    groups = listOf("Added", "Changed", "Deprecated", "Removed", "Fixed", "Security")
-//}
-
-
-// https://github.com/bintray/gradle-bintray-plugin#readme
-bintray {
-    user = System.getenv("BINTRAY_USER")
-    key = System.getenv("BINTRAY_API_KEY")
-
-//    configurations = ["archives"] // Upload the archives to bintray
-
-    dryRun = true //[Default: false] Whether to run this as dry-run, without deploying
-    publish = false //[Default: false] Whether version should be auto published after an upload
-    override = false //[Default: false] Whether to override version artifacts already published
-
-    //Package configuration. The plugin will use the repo and name properties to check if the package already exists. In that case, there"s no need to configure the other package properties (like userOrg, desc, etc).
-    pkg(delegateClosureOf<BintrayExtension.PackageConfig> {
-        repo = "JavaParser-AST-Inspector"
-        name = "${project.group}:${project.name}"
-
-        setLicenses("MIT")
-
-        websiteUrl = "https://github.com/MysterAitch/JavaParser-AST-Inspector"
-        issueTrackerUrl = "https://github.com/MysterAitch/JavaParser-AST-Inspector/issues"
-        vcsUrl = "https://github.com/MysterAitch/JavaParser-AST-Inspector.git"
-
-        githubRepo = "MysterAitch/JavaParser-AST-Inspector" //Optional Github repository
-        githubReleaseNotesFile = "CHANGELOG.md" //Optional Github readme file
-
-        with(version) {
-            name = artifactVersion
-        }
-    })
 }
