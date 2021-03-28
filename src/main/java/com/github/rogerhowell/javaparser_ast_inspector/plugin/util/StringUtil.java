5cc958ceda31f3ccd78192c15507498cfd8b249c
Roger Howell <MysterAitch@users.noreply.github.com>
2021-03-28 05:49:32 +0100

    use StringBuilder instead of string concatenation within StringUtil and include additional tests

--- src/main/java/com/github/rogerhowell/javaparser_ast_inspector/plugin/util/StringUtil.java
+++ src/main/java/com/github/rogerhowell/javaparser_ast_inspector/plugin/util/StringUtil.java
@@ -6,27 +6,42 @@ public final class StringUtil {
     private StringUtil() {
     }
 
+    /**
+     * @param input                The text to have padding applied.
+     * @param desiredMinimumLength The minimum length of the output.
+     * @return When the input length is > desired minimum length, the input.
+     * Otherwise the input padded with spaces at the END, up to the desiredMinimumLength
+     */
     public static String padEnd(String input, int desiredMinimumLength) {
-        if(input.length() > desiredMinimumLength) {
+        if (input.length() > desiredMinimumLength) {
             return input;
         }
 
-        while(input.length() < desiredMinimumLength) {
-            input = input + " ";
+        StringBuilder inputBuilder = new StringBuilder(input);
+        while (inputBuilder.length() < desiredMinimumLength) {
+            inputBuilder.append(" ");
         }
 
-        return input;
+        return inputBuilder.toString();
     }
+
+    /**
+     * @param input                The text to have padding applied.
+     * @param desiredMinimumLength The minimum length of the output.
+     * @return When the input length is > desired minimum length, the input.
+     * Otherwise the input padded with spaces at the START, up to the desiredMinimumLength
+     */
     public static String padStart(String input, int desiredMinimumLength) {
-        if(input.length() > desiredMinimumLength) {
+        if (input.length() > desiredMinimumLength) {
             return input;
         }
 
-        while(input.length() < desiredMinimumLength) {
-            input = " " + input;
+        StringBuilder inputBuilder = new StringBuilder(input);
+        while (inputBuilder.length() < desiredMinimumLength) {
+            inputBuilder.insert(0, " ");
         }
 
-        return input;
+        return inputBuilder.toString();
     }
 
 }
--- src/test/java/com/github/rogerhowell/javaparser_ast_inspector/plugin/util/StringUtilTest.java
+++ src/test/java/com/github/rogerhowell/javaparser_ast_inspector/plugin/util/StringUtilTest.java
@@ -47,4 +47,46 @@ public class StringUtilTest {
         String output = StringUtil.padEnd(input, 10);
         Assert.assertEquals("ABC       ", output);
     }
+    @Test
+    public void padStartTest0() {
+        String input = "ABC";
+        String output = StringUtil.padStart(input, 0);
+        Assert.assertEquals("ABC", output);
+    }
+    @Test
+    public void padStartTest1() {
+        String input = "ABC";
+        String output = StringUtil.padStart(input, 1);
+        Assert.assertEquals("ABC", output);
+    }
+    @Test
+    public void padStartTest2() {
+        String input = "ABC";
+        String output = StringUtil.padStart(input, 2);
+        Assert.assertEquals("ABC", output);
+    }
+    @Test
+    public void padStartTest3() {
+        String input = "ABC";
+        String output = StringUtil.padStart(input, 3);
+        Assert.assertEquals("ABC", output);
+    }
+    @Test
+    public void padStartTest4() {
+        String input = "ABC";
+        String output = StringUtil.padStart(input, 4);
+        Assert.assertEquals(" ABC", output);
+    }
+    @Test
+    public void padStartTest5() {
+        String input = "ABC";
+        String output = StringUtil.padStart(input, 5);
+        Assert.assertEquals("  ABC", output);
+    }
+    @Test
+    public void padStartTest10() {
+        String input = "ABC";
+        String output = StringUtil.padStart(input, 10);
+        Assert.assertEquals("       ABC", output);
+    }
 }
