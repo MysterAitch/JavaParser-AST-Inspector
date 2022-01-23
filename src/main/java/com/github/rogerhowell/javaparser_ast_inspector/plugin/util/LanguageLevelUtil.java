880999608879b4a3b936e5041451d3cd60996c0f
Roger Howell <MysterAitch@users.noreply.github.com>
2022-01-23 01:55:15 +0000

    change language level util implementation, and improved tests

--- src/main/java/com/github/rogerhowell/javaparser_ast_inspector/plugin/util/LanguageLevelUtil.java
+++ src/main/java/com/github/rogerhowell/javaparser_ast_inspector/plugin/util/LanguageLevelUtil.java
@@ -43,23 +43,38 @@ public final class LanguageLevelUtil {
     @NotNull
     public static Optional<ParserConfiguration.LanguageLevel> mapStringLanguageLevelToJavaParserLanguageLevel(final String languageLevelName) {
         final HashMap<String, ParserConfiguration.LanguageLevel> languageLevelMap = new HashMap<>();
+
+        /*
+         * Iterate over all known JavaParser language levels.
+         * Note that this will include preview versions too.
+         */
         for (ParserConfiguration.LanguageLevel javaParserLanguageLevel : ParserConfiguration.LanguageLevel.values()) {
-            // JavaParser uses `JAVA_{}`, while IntelliJ uses `JDK_{}`.
-            String javaParserLanguageLevelName = javaParserLanguageLevel.name();
-            languageLevelMap.put(javaParserLanguageLevelName, javaParserLanguageLevel);
+
+            String plainLanguageLevelName = javaParserLanguageLevel
+                    .name()
+                    .replace("JAVA_1_", "JAVA_")
+                    .replace("JAVA_", "");
+
+
+            // No prefix
+            languageLevelMap.put(plainLanguageLevelName, javaParserLanguageLevel);
+            // JavaParser uses `JAVA_{}`
+            languageLevelMap.put("JAVA_" + plainLanguageLevelName, javaParserLanguageLevel);
+            // IntelliJ uses `JDK_{}`.
+            languageLevelMap.put("JDK_" + plainLanguageLevelName, javaParserLanguageLevel);
 
             // Some versions have a `1.` prefix (e.g. JDK 6 is also known as 1.6, thus is listed as JDK_1_6 as opposed to JDK_6).
-            String javaParserLanguageLevelNameWith1_Prefix = javaParserLanguageLevel.name().replace("JAVA_", "JAVA_1_");
-            languageLevelMap.put(javaParserLanguageLevelNameWith1_Prefix, javaParserLanguageLevel);
-
-            // JavaParser uses `JAVA_{}`, while IntelliJ uses `JDK_{}`.
-            String intellijLanguageLevelName = javaParserLanguageLevel.name().replace("JAVA_", "JDK_");
-            languageLevelMap.put(intellijLanguageLevelName, javaParserLanguageLevel);
-
-            // Some versions have a `1.` prefix (e.g. JDK 6 is also known as 1.6, thus is listed as JDK_1_6 as opposed to JDK_6).
-            String intellijLanguageLevelNameWith1_Prefix = javaParserLanguageLevel.name().replace("JAVA_", "JDK_1_");
-            languageLevelMap.put(intellijLanguageLevelNameWith1_Prefix, javaParserLanguageLevel);
+            // This is a bit of a hacky catch-all, given that not all places are consistent (e.g. JP has JAVA_6, but IntelliJ has JDK_1_6
+            languageLevelMap.put("JAVA_1_" + plainLanguageLevelName, javaParserLanguageLevel);
+            languageLevelMap.put("JDK_1_" + plainLanguageLevelName, javaParserLanguageLevel);
         }
+
+        // IntelliJ also contains "JDK X" which includes "experimental features" - i.e., latest preview
+        languageLevelMap.put("X", ParserConfiguration.LanguageLevel.BLEEDING_EDGE);
+        languageLevelMap.put("JAVA_X", ParserConfiguration.LanguageLevel.BLEEDING_EDGE);
+        languageLevelMap.put("JDK_X", ParserConfiguration.LanguageLevel.BLEEDING_EDGE);
+        languageLevelMap.put("JAVA_1_X", ParserConfiguration.LanguageLevel.BLEEDING_EDGE);
+        languageLevelMap.put("JDK_1_X", ParserConfiguration.LanguageLevel.BLEEDING_EDGE);
 
         ParserConfiguration.LanguageLevel selectedLanguageLevel = languageLevelMap.get(languageLevelName);
         return Optional.ofNullable(selectedLanguageLevel);
