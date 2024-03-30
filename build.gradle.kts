7d1f13527badbe513fd3accc727b148814462c5f
TomXin <766781886@qq.com>
2024-03-30 13:18:51 +0800

    Update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,3 @@
-import io.gitlab.arturbosch.detekt.Detekt
 import org.jetbrains.changelog.Changelog
 import org.jetbrains.changelog.markdownToHTML
 
@@ -6,41 +5,30 @@ fun properties(key: String) = providers.gradleProperty(key)
 fun environment(key: String) = providers.environmentVariable(key)
 
 plugins {
-    // Java support
-    id("java")
-    // Kotlin support
-    id("org.jetbrains.kotlin.jvm") version "1.9.0"
-    // Gradle IntelliJ Plugin - read more: https://github.com/JetBrains/gradle-intellij-plugin
-    id("org.jetbrains.intellij") version "1.15.0"
-    // Gradle Changelog Plugin - read more: https://github.com/JetBrains/gradle-changelog-plugin
-    id("org.jetbrains.changelog") version "2.1.2"
-    // Gradle Qodana Plugin
-    id("org.jetbrains.qodana") version "0.1.13"
-    // detekt linter - read more: https://detekt.github.io/detekt/gradle.html
-    id("io.gitlab.arturbosch.detekt") version "1.20.0"
-    // ktlint linter - read more: https://github.com/JLLeitschuh/ktlint-gradle
-    id("org.jlleitschuh.gradle.ktlint") version "11.6.0"
-
-    id("jacoco")
+    id("java") // Java support
+    alias(libs.plugins.kotlin) // Kotlin support
+    alias(libs.plugins.gradleIntelliJPlugin) // Gradle IntelliJ Plugin
+    alias(libs.plugins.changelog) // Gradle Changelog Plugin
+    alias(libs.plugins.qodana) // Gradle Qodana Plugin
+    alias(libs.plugins.kover) // Gradle Kover Plugin
 }
 
 group = properties("pluginGroup").get()
 version = properties("pluginVersion").get()
 
-val javaParserVersion = "3.25.5"
+val javaParserVersion = "3.25.9"
 
 // Configure project's dependencies
 repositories {
     mavenCentral()
 }
 dependencies {
-    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.20.0")
 
-    implementation("com.github.javaparser:javaparser-symbol-solver-core:$javaParserVersion"){
+    implementation("com.github.javaparser:javaparser-symbol-solver-core:$javaParserVersion") {
         exclude(group = "com.google.guava")
         exclude(group = "org.javassist")
     }
-    implementation("org.apache.commons:commons-text:1.10.0"){
+    implementation("org.apache.commons:commons-text:1.11.0") {
         exclude(group = "*")
     }
     // implementation("guru.nidi:graphviz-java-all-j2v8:0.18.1")
@@ -51,60 +39,46 @@ dependencies {
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.0")
 }
 
-// Set the JVM language level used to build the project. Use Java 11 for 2020.3+, and Java 17 for 2022.2+.
+// Set the JVM language level used to build the project.
 kotlin {
     jvmToolchain(17)
 }
 
-// Configure plugin `Gradle IntelliJ Plugin`.
-// Read more: https://github.com/JetBrains/gradle-intellij-plugin
+// Configure Gradle IntelliJ Plugin - read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-    pluginName.set(properties("pluginName"))
-    version.set(properties("platformVersion"))
-    type.set(properties("platformType"))
+    pluginName = properties("pluginName")
+    version = properties("platformVersion")
+    type = properties("platformType")
     downloadSources = true
 //    downloadSources = properties("platformDownloadSources")
     updateSinceUntilBuild = true
 
     // Plugin Dependencies. Uses `platformPlugins` property from the gradle.properties file.
     plugins = properties("platformPlugins").map { it.split(',').map(String::trim).filter(String::isNotEmpty) }
+    plugins.add(file("D:/dev_soft/tools"))
 }
-// Configure plugin `Gradle Changelog Plugin`.
-// Read more: https://github.com/JetBrains/gradle-changelog-plugin
+
+// Configure Gradle Changelog Plugin - read more: https://github.com/JetBrains/gradle-changelog-plugin
 changelog {
     groups.empty()
     repositoryUrl = properties("pluginRepositoryUrl")
 }
 
-// Configure Gradle Qodana Plugin - read more: https://github.com/JetBrains/gradle-qodana-plugin
-qodana {
-    cachePath = provider { file(".qodana").canonicalPath }
-    reportPath = provider { file("build/reports/inspections").canonicalPath }
-    saveReport = true
-    showReport = environment("QODANA_SHOW_REPORT").map { it.toBoolean() }.getOrElse(false)
-}
-
-// Configure detekt plugin.
-// Read more: https://detekt.github.io/detekt/kotlindsl.html
-detekt {
-    config = files("./detekt-config.yml")
-    buildUponDefaultConfig = true
-}
-
-tasks.withType<Detekt>().configureEach {
-    reports {
-        xml.required.set(true)
-        xml.required.set(true)
-        txt.required.set(true)
+// Configure Gradle Kover Plugin - read more: https://github.com/Kotlin/kotlinx-kover#configuration
+koverReport {
+    defaults {
+        xml {
+            onCheck = true
+        }
     }
 }
 
 tasks {
-    initializeIntelliJPlugin{
+    initializeIntelliJPlugin {
         offline = true
     }
 
-    buildSearchableOptions{
+    buildSearchableOptions {
         enabled = false
     }
 
@@ -128,7 +102,7 @@ tasks {
                 }
                 subList(indexOf(start) + 1, indexOf(end))
                     .joinToString("\n")
-                    .replace(Regex("v\\d+\\.\\d+\\.\\d+"), "3.25.5")
+                    .replace(Regex("v\\d+\\.\\d+\\.\\d+"), "3.25.9")
                     .let(::markdownToHTML)
             }
         }
@@ -141,14 +115,10 @@ tasks {
                     (getOrNull(pluginVersion) ?: getUnreleased())
                         .withHeader(false)
                         .withEmptySections(false),
-                    Changelog.OutputType.HTML
+                    Changelog.OutputType.HTML,
                 )
             }
         }
-    }
-
-    runPluginVerifier {
-        ideVersions = properties("pluginVerifierIdeVersions").map { it.split(',').map(String::trim).filter(String::isNotEmpty) }
     }
 
     // Configure UI tests plugin
@@ -172,27 +142,14 @@ tasks {
         // The pluginVersion is based on the SemVer (https://semver.org) and supports pre-release labels, like 2.1.7-alpha.3
         // Specify pre-release label to publish the plugin in a custom Release Channel automatically. Read more:
         // https://plugins.jetbrains.com/docs/intellij/deployment.html#specifying-a-release-channel
-        channels = properties("pluginVersion").map { listOf(it.split('-').getOrElse(1) { "default" }.split('.').first()) }
+        channels = properties("pluginVersion").map { listOf(it.substringAfter('-', "").substringBefore('.').ifEmpty { "default" }) }
     }
-}
-
-tasks.jacocoTestReport {
-    reports {
-        xml.required.set(true)
-        csv.required.set(true)
-        html.required.set(true)
-    }
-}
-
-// https://docs.gradle.org/current/dsl/org.gradle.api.tasks.testing.Test.html
-// https://docs.gradle.org/current/userguide/java_testing.html#using_junit5
-tasks.test {
-    // enable JUnit Platform (a.k.a. JUnit 5) support
-    useJUnitPlatform()
-
-    finalizedBy("jacocoTestReport")
-    doLast {
-        logger.info("View code coverage at:")
-        logger.info("file://${layout.buildDirectory}/reports/jacoco/test/html/index.html")
+    runIde {
+        systemProperties["idea.is.internal"] = true
+        jvmArgs = listOf(
+            "-javaagent:D:/dev_soft/jetbra/ja-netfilter.jar=jetbrains",
+            "--add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED",
+            "--add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED"
+        )
     }
 }
